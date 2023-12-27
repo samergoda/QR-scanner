72925cec5fdf78c5c5813abfb2e0a1e4886d87ed
samergoda <113331227+samergoda@users.noreply.github.com>
2023-12-27 21:25:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 
- let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-  
- scanner.addListener('scan', function(content) {
-   console.log('Scanned content: ' + content);
-   document.getElementById('result').textContent = content;
- });
+scanner.addListener('scan', function(content) {
+  console.log('Scanned content: ' + content);
+ 
+});
 
- Instascan.Camera.getCameras().then(function(cameras) {
-   if (cameras.length > 0) {
-     scanner.start(cameras[0]);
-   } else {
-     console.error('No cameras found.');
-     
-   }
- }).catch(function(e) {
-   console.error(e);
+Instascan.Camera.getCameras().then(function(cameras) {
+  if (cameras.length > 0) {
+    
+    let backCamera = cameras.find(function(camera) {
+      return camera.name.toLowerCase().includes('back');
+    });
+
+    scanner.start(backCamera || cameras[0]);
+  } else {
+    console.error('No cameras found.');
+  }
+}).catch(function(e) {
+  console.error(e);
   });
