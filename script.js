47cd2e3841d011f6c07613e22aab370c64658bf0
samergoda <113331227+samergoda@users.noreply.github.com>
2023-12-27 21:37:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
+ let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+  
+ scanner.addListener('scan', function(content) {
+   console.log('Scanned content: ' + content);
+   document.getElementById('result').textContent = content;
+ });
+
+ Instascan.Camera.getCameras().then(function(cameras) {
+   if (cameras.length > 0) {
+     scanner.start(cameras[1]);
+   } else {
+     console.error('No cameras found.');
+     
+   }
+ }).catch(function(e) {
+   console.error(e);
+  });
+
 // let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 
 // scanner.addListener('scan', function(content) {
 //   console.log('Scanned content: ' + content);
- 
+//   document.getElementById('result').textContent = content;
 // });
 
 // Instascan.Camera.getCameras().then(function(cameras) {
 //   if (cameras.length > 0) {
-    
-//     let backCamera = cameras.find(function(camera) {
-//       return camera.name.toLowerCase().includes('back');
-//     });
+//     // Find a camera by checking if it is not labeled as the front camera
+//     const backCamera = cameras.find(camera => !camera.name.toLowerCase().includes('front'));
 
+//     // If a back camera is found, start scanning with it; otherwise, use the first available camera
 //     scanner.start(backCamera || cameras[0]);
 //   } else {
 //     console.error('No cameras found.');
 //   }
 // }).catch(function(e) {
 //   console.error(e);
-//   });
-let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-
-scanner.addListener('scan', function(content) {
-  console.log('Scanned content: ' + content);
-  document.getElementById('result').textContent = content;
-});
-
-Instascan.Camera.getCameras().then(function(cameras) {
-  if (cameras.length > 0) {
-    // Find a camera by checking if it is not labeled as the front camera
-    const backCamera = cameras.find(camera => !camera.name.toLowerCase().includes('front'));
-
-    // If a back camera is found, start scanning with it; otherwise, use the first available camera
-    scanner.start(backCamera || cameras[0]);
-  } else {
-    console.error('No cameras found.');
-  }
-}).catch(function(e) {
-  console.error(e);
-});
+// });
 
