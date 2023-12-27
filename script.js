46fd8df31129b8c15db1022189fbd07c2c2dee8b
samergoda <113331227+samergoda@users.noreply.github.com>
2023-12-27 21:31:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+// let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+
+// scanner.addListener('scan', function(content) {
+//   console.log('Scanned content: ' + content);
+ 
+// });
+
+// Instascan.Camera.getCameras().then(function(cameras) {
+//   if (cameras.length > 0) {
+    
+//     let backCamera = cameras.find(function(camera) {
+//       return camera.name.toLowerCase().includes('back');
+//     });
+
+//     scanner.start(backCamera || cameras[0]);
+//   } else {
+//     console.error('No cameras found.');
+//   }
+// }).catch(function(e) {
+//   console.error(e);
+//   });
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 
 scanner.addListener('scan', function(content) {
   console.log('Scanned content: ' + content);
- 
+  document.getElementById('result').textContent = content;
 });
 
 Instascan.Camera.getCameras().then(function(cameras) {
   if (cameras.length > 0) {
-    
-    let backCamera = cameras.find(function(camera) {
-      return camera.name.toLowerCase().includes('back');
-    });
+    // Find the back camera (position: 'environment')
+    const backCamera = cameras.find(camera => camera.name.toLowerCase().includes('back') || camera.position === 'environment');
 
+    // If a back camera is found, start scanning with it; otherwise, use the first available camera
     scanner.start(backCamera || cameras[0]);
   } else {
     console.error('No cameras found.');
   }
 }).catch(function(e) {
   console.error(e);
-  });
+});
